a99f32212b33af882fc2e00266e44accf80c8000
Kaisar <myrzakhmet.kaisar@gmail.com>
2022-02-13 17:03:36 +0600

    added punctuation

--- src/components/info.js
+++ src/components/info.js
@@ -5,7 +5,7 @@ import React from "react";
   const Info = () => {
       return (
         <div>
-           <h2>Weather app</h2>
+           <h2>Weather app!</h2>
            <p>Knowling weather in your city </p>
         </div>
       );
